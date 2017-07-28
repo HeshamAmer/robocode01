ea41ae4cff15dbbb0b94f0306b7fc0df74ac0299
Hesham Amer <hesham.amer@metrosystems.net>
2017-07-28 16:35:40 +0200

    Distance is noooooooooooooooooo problem!

--- src/main/java/de/metro/robocode/IWillFeastOnYourFarts.java
+++ src/main/java/de/metro/robocode/IWillFeastOnYourFarts.java
@@ -1,15 +1,16 @@
 package de.metro.robocode;
 
+import java.awt.Color;
 import robocode.*;
-import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 public class IWillFeastOnYourFarts extends Robot {
     double radius = 100.0;
     double angle = 90.0;
     boolean directionLeft = true;
     double radarBearing;
-    boolean isGunBehind = false;
+    boolean disengageSpam = false;
     boolean gunMovingEnabled = false;
+
     @Override
     public void run() {
 
@@ -27,11 +28,17 @@ public class IWillFeastOnYourFarts extends Robot {
         setDebugProperty( "ScannedRobotEvent", String.valueOf( e ) );
 
         radarBearing = e.getBearing();
-
+        if ( radarBearing >=0 && radarBearing <= 5)
+            fireForBearingBetween( 0, 10 );
         setDebugProperty( "Distance", String.valueOf( e.getDistance() ) );
-        if (Math.abs(  radarBearing ) >= 80  && Math.abs( radarBearing ) <= 100) {
-            if (e.getDistance() < 330)
-                fireBullet( 50 );
+        //if (e.getDistance() < 330)
+            fireForBearingBetween(80,100);
+    }
+
+    private void fireForBearingBetween( final int i, final int i1 ) {
+        if (Math.abs(  radarBearing ) >= i && Math.abs( radarBearing ) <= i1) {
+
+            fireBullet( 50 );
         }
     }
 
@@ -41,14 +48,41 @@ public class IWillFeastOnYourFarts extends Robot {
     public void onHitWall(HitWallEvent e) {
         final double bearing = e.getBearing();
         turnLeft(90-bearing );
-        if (gunMovingEnabled == false) {
+        if ( !gunMovingEnabled ) {
             turnGunLeft( 90 );
             //turnRadarLeft( 90 );
             gunMovingEnabled = true;
+            setBodyColor( Color.PINK );
+            setGunColor( Color.black );
+            setRadarColor( Color.yellow );
+            setBulletColor( Color.pink );
         }
 
     }
+
+    @Override
+    public void onBulletMissed( final BulletMissedEvent event ) {
+        disengageSpam = true;
+        super.onBulletMissed( event );
+    }
+
+    @Override
+    public void onBulletHit( final BulletHitEvent event ) {
+        disengageSpam = false;
+        super.onBulletHit( event );
+    }
+
+
     public void onHitRobot(HitRobotEvent e) {
+
+        turnGunRight( 90 );
+
+        int times = (int) ( e.getEnergy()/3 );
+        while (times >=0 && !disengageSpam) {
+            fireBullet(1000000  );
+            times --;
+        }
+        turnGunLeft( 90 );
         back( radius/2 );
         turnLeft(45 );
     }
